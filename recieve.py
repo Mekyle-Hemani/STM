24498f54be7e72cedb9b2a5b037e2e38d100dc03
Mekyle-Hemani <153373890+Mekyle-Hemani@users.noreply.github.com>
2025-03-08 15:52:34 -0500

    Update recieve.py

--- recieve.py
+++ recieve.py
@@ -1,13 +1,18 @@
 import time
-import imager
+import serial
+import serial.tools.list_ports
 
 doneImage=False
 data = []
 
 def setup():
+    ports = list(serial.tools.list_ports.comports())
+    com_ports = [port.device for port in ports]
+    #Establish communication with the first COM port
     return True
 
 def startSTM():
+    #Turn the onboard LED on
     return True
 
 def checkElectronDistanceProcess():
@@ -18,3 +23,5 @@ def checkElectronDistanceProcess():
 
     import random
     data = [[random.uniform(0, 5) for _ in range(90)] for _ in range(160)]
+
+    #Turn the onboard LED on
