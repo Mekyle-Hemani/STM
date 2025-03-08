e420ab170b2ffa3e9c61641358cd5de1e57a6684
Mekyle-Hemani <153373890+Mekyle-Hemani@users.noreply.github.com>
2025-03-08 15:52:41 -0500

    Update initAll.py

--- initialization/initAll.py
+++ initialization/initAll.py
@@ -4,14 +4,16 @@ if os.name != "nt":
     import initialization.environmentCheck
 
 def checkAll():
+    result=True
     if os.name != "nt":
         initialization.environmentCheck.checkEnvironment()
     else:
         import colourprint
         colourprint.print_colored("OS is Windows. Environment dependencies installations skipped...", colourprint.ORANGE)
 
-    dependencies = ["tkinter", "serial"]
-    result=True
+    dependencies = ["tkinter", "serial", "serial.tools.list_ports"]
+    exceptions = {"serial.tools.list_ports": "pyserial"}
+    
     for item in dependencies:
-        initialization.packageCheck.checkPackage(item)
+        initialization.packageCheck.checkPackage(item, exceptions)
     return result
